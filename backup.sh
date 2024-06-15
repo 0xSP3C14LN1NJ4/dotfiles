c4c2657a710ba52334c7d7fc72b2ca3c3965e99d
0xSP3C14LN1NJ4 <0xSP3C14LN1NJ4@gmail.com>
2024-06-15 17:34:41 -0400

    Add backup script

---
+++ backup.sh
@@ -0,0 +1,1 @@
+rsync -aAXH --info=progress2 --exclude='/dev/*' --exclude='/proc/*' --exclude='/sys/*' --exclude='/tmp/*' --exclude='/run/*' --exclude='/mnt/*' --exclude='/media/*' --exclude='/lost+found/' --exclude='/home/*/.cache' --exclude='/var/cache/' / /mnt/sdb1
